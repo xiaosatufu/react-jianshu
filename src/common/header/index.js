59e230e4d2b9fd0f4c4fc9b09d7d3a23a8641ec1
yongshuang <yongshuang@gdgyun.com>
2019-01-02 10:57:28 +0800

    combineReducers

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -59,7 +59,7 @@ const Header = props => {
 
 const mapStateToProps = state => {
   return {
-    focused: state.focused
+    focused: state.header.focused
   };
 };
 const mapDispatchToProps = dispatch => {
