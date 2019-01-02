2fdd0dd4c90536bef3a5ad2c75dc362408fef65f
yongshuang <yongshuang@gdgyun.com>
2019-01-02 10:39:12 +0800

    redux react-redux

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,53 +1,85 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 import "./index.scss";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      focused: false
-    };
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-  }
-  render() {
-    return (
-      <div className="header-wrapper">
-        <a href="/" className="logo" />
-        <div className="nav">
-          <div className="navItem left active">首页</div>
-          <div className="navItem left">下载App</div>
-          <div className="navItem right">登陆</div>
-          <div className="navItem right">Aa</div>
-          <input
-            onFocus={this.handleInputFocus}
-            onBlur={this.handleInputBlur}
-            type="text"
-            className={`navSearch ${this.state.focused ? "focus" : ""}`}
-            placeholder="搜索"
-          /> 
-        </div>
-        <div className="addition">
-          <a href="#" className="writting">
-            写文章
-          </a>
-          <a href="#" className="reg">
-            注册
-          </a>
-        </div>
+const Header = props => {
+  return (
+    <div className="header-wrapper">
+      <a href="/" className="logo" />
+      <div className="nav">
+        <div className="navItem left active">首页</div>
+        <div className="navItem left">下载App</div>
+        <div className="navItem right">登陆</div>
+        <div className="navItem right">Aa</div>
+        <input
+          onFocus={props.handleInputFocus}
+          onBlur={props.handleInputBlur}
+          type="text"
+          className={`navSearch ${props.focused ? "focus" : ""}`}
+          placeholder="搜索"
+        />
       </div>
-    );
-  }
-  handleInputFocus() {
-    this.setState({
-      focused: true
-    });
-  }
-  handleInputBlur() {
-    this.setState({
-      focused: false
-    });
-  }
-}
+      <div className="addition">
+        <a href="#" className="writting">
+          写文章
+        </a>
+        <a href="#" className="reg">
+          注册
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Header;
+// class Header extends Component {
+//   constructor(props) {
+//     super(props);
+//     // this.state = {
+//     //   focused: false
+//     // };
+//     // this.handleInputFocus = this.handleInputFocus.bind(this);
+//     // this.handleInputBlur = this.handleInputBlur.bind(this);
+//   }
+//   render() {
+//     return (
+//     );
+//   }
+//   // handleInputFocus() {
+//   //   this.setState({
+//   //     focused: true
+//   //   });
+//   // }
+//   // handleInputBlur() {
+//   //   this.setState({
+//   //     focused: false
+//   //   });
+//   // }
+// }
+
+const mapStateToProps = state => {
+  return {
+    focused: state.focused
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: "search_focus"
+      };
+      dispatch(action);
+    },
+    handleInputBlur() {
+      const action = {
+        type: "search_blur"
+      };
+      dispatch(action);
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
